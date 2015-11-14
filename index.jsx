634af2cea811519c28fbff015600b3e6d256d1b6
terakilobyte <terakilobyte@gmail.com>
2015-11-13 17:40:43 -0800

    Initial commit

---
+++ index.jsx
@@ -0,0 +1,12 @@
+require("./node_modules/bootstrap/dist/css/bootstrap.min.css")
+import React from 'react';
+
+export class App extends React.Component {
+	render() {
+		return (
+        <div>Simple React + Babel + Bootstrap + Webpack </div>
+		);
+	}
+}
+
+React.render(<App/>, document.querySelector("#myApp"));
